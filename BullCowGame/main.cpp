33f2f230b43258972b0c0b9fd0f0a3f93a593145
Gabi <gabifijalkowska@hotmail.com>
2018-01-12 18:04:06 +0100

    Added first word game class

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "BullCowGame.h"
+#include <iostream>
+#include <string>
+
+void PrintIntro();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame;
+
+int main()
+{
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
+
+	return 0;
+}
+
+void PrintIntro()
+{
+	constexpr int WORLD_LENGTH = 9;
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORLD_LENGTH;
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
+
+
+void PlayGame()
+{
+	int MaxTries = BCGame.GetMaxTries();
+
+	for (int count = 1; count <= MaxTries; count++) {
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+
+std::string GetGuess()
+{
+	int CurrentTry = BCGame.GetCurrentTry();
+
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again (y/n)? ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
